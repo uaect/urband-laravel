80fdb1ebc94be594de666e161ea23996ab49cf12
Shejin Abu <shejin.abu@gmail.com>
2019-09-11 18:15:13 +0400

    Updates

---
+++ precache-manifest.ad0abb7b7606bfae17c73b482721d32d.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c798d2bf2596e2d0c2f7961a55c98f2d",
+    "url": "/urband/index.html"
+  },
+  {
+    "revision": "c9f4d0cbb52d273451a1",
+    "url": "/urband/static/css/2.cb4a70f0.chunk.css"
+  },
+  {
+    "revision": "824f67d7e9e4d2f869f8",
+    "url": "/urband/static/css/main.319c8d3e.chunk.css"
+  },
+  {
+    "revision": "c9f4d0cbb52d273451a1",
+    "url": "/urband/static/js/2.0bbaf9a9.chunk.js"
+  },
+  {
+    "revision": "824f67d7e9e4d2f869f8",
+    "url": "/urband/static/js/main.7c80170a.chunk.js"
+  },
+  {
+    "revision": "542eabc7ffcf143d808a",
+    "url": "/urband/static/js/runtime~main.662696d4.js"
+  },
+  {
+    "revision": "30c19b25b9d071a7c766eade1d30e9c7",
+    "url": "/urband/static/media/1.30c19b25.jpg"
+  },
+  {
+    "revision": "63d16ac443f9ee9ee95cc2fc640af113",
+    "url": "/urband/static/media/1.63d16ac4.png"
+  },
+  {
+    "revision": "a6b054d85892c153762056b832b0a201",
+    "url": "/urband/static/media/1.a6b054d8.png"
+  },
+  {
+    "revision": "1d4f4b1635b9b1a797004180948afbb8",
+    "url": "/urband/static/media/10.1d4f4b16.jpg"
+  },
+  {
+    "revision": "eb055335f0fa65855dff3483fc09c0dc",
+    "url": "/urband/static/media/10.eb055335.jpg"
+  },
+  {
+    "revision": "5663827e624c74bf99fba1c4e4394534",
+    "url": "/urband/static/media/11.5663827e.jpg"
+  },
+  {
+    "revision": "7ff979ad8269a15dfd2971c5af60fdb3",
+    "url": "/urband/static/media/12.7ff979ad.jpg"
+  },
+  {
+    "revision": "4e71abca86edd0fd89c66bb45b487fa2",
+    "url": "/urband/static/media/13.4e71abca.jpg"
+  },
+  {
+    "revision": "8167f5229ef4fe41cc4be86f53b7df13",
+    "url": "/urband/static/media/13.8167f522.png"
+  },
+  {
+    "revision": "02aa1e3335c0ba689809bd55f0ee8bcd",
+    "url": "/urband/static/media/18.02aa1e33.jpg"
+  },
+  {
+    "revision": "f1fee5a07d24509ced96583f1932f5e5",
+    "url": "/urband/static/media/19.f1fee5a0.jpg"
+  },
+  {
+    "revision": "e77061fb631c5244b0e51b257218f5dc",
+    "url": "/urband/static/media/2.e77061fb.png"
+  },
+  {
+    "revision": "e2402ce6cf2f749311ec43ac9c3cb63f",
+    "url": "/urband/static/media/20.e2402ce6.jpg"
+  },
+  {
+    "revision": "84b11b37bd03ee37699069f8400df150",
+    "url": "/urband/static/media/3.84b11b37.png"
+  },
+  {
+    "revision": "cc4f05fc820e1df11e9b32f195c5ba09",
+    "url": "/urband/static/media/3.cc4f05fc.png"
+  },
+  {
+    "revision": "60817ba8dc593df37b2d300eb8fc8e45",
+    "url": "/urband/static/media/4.60817ba8.jpg"
+  },
+  {
+    "revision": "ab99e26c2772a9f39a09d2cf7e54ea8c",
+    "url": "/urband/static/media/4.ab99e26c.png"
+  },
+  {
+    "revision": "df00e2e332d5d9999ee976a0997dfd59",
+    "url": "/urband/static/media/5.df00e2e3.png"
+  },
+  {
+    "revision": "056c64a893111fa49eec24e4a3e4bc19",
+    "url": "/urband/static/media/7.056c64a8.jpg"
+  },
+  {
+    "revision": "60508c1d739bbd4fbc02d9095aa83899",
+    "url": "/urband/static/media/7.60508c1d.png"
+  },
+  {
+    "revision": "e64e0ea539a863f45390eb2017c9bf93",
+    "url": "/urband/static/media/7.e64e0ea5.jpg"
+  },
+  {
+    "revision": "05aa22e3da2cf0f94a75ab364531d753",
+    "url": "/urband/static/media/8.05aa22e3.png"
+  },
+  {
+    "revision": "f3e92a38d8e61fe87c9b4f2442846c9f",
+    "url": "/urband/static/media/8.f3e92a38.jpg"
+  },
+  {
+    "revision": "deb70863ed969bea8f4ab7cd947a1e8c",
+    "url": "/urband/static/media/9.deb70863.jpg"
+  },
+  {
+    "revision": "ed76407e93cdf13c0e99b06a6f51f8d9",
+    "url": "/urband/static/media/9.ed76407e.jpg"
+  },
+  {
+    "revision": "0f4c6b1734c7a0e7d23356b8b1131c2e",
+    "url": "/urband/static/media/bg5.0f4c6b17.jpg"
+  },
+  {
+    "revision": "dc1225cda87e13d222ad49d52800b5dc",
+    "url": "/urband/static/media/brushbg.dc1225cd.png"
+  },
+  {
+    "revision": "6ddedf3cd6544769cc1472a1ea360dd0",
+    "url": "/urband/static/media/f1.6ddedf3c.jpg"
+  },
+  {
+    "revision": "3743c7aa2cd96ec6fe5ac222e826364a",
+    "url": "/urband/static/media/f3.3743c7aa.jpg"
+  },
+  {
+    "revision": "5e1364534a8dcb4c50b09cc56e1ebe1a",
+    "url": "/urband/static/media/f8.5e136453.jpg"
+  },
+  {
+    "revision": "370255ff1fcbb163b29bcdbabf4178e7",
+    "url": "/urband/static/media/footer-bg.370255ff.jpg"
+  },
+  {
+    "revision": "9f4d307124222b884cc9e3eba31a0734",
+    "url": "/urband/static/media/intro_inline-webfont.9f4d3071.woff2"
+  },
+  {
+    "revision": "c2ce3472b4d66f47effc0cc0b1e29625",
+    "url": "/urband/static/media/intro_inline-webfont.c2ce3472.woff"
+  },
+  {
+    "revision": "c078ab7cdcf651c8892d7bc49f98c9c2",
+    "url": "/urband/static/media/mouse-move.c078ab7c.png"
+  },
+  {
+    "revision": "ffb508097f12c2c6ba878106a4d15684",
+    "url": "/urband/static/media/offset-menu-bg.ffb50809.png"
+  },
+  {
+    "revision": "7bb7bc90691dafe6a9e7fb7011105ab6",
+    "url": "/urband/static/media/player-key.7bb7bc90.png"
+  },
+  {
+    "revision": "fe1f25459517c8506e86036afb77874b",
+    "url": "/urband/static/media/record-shade.fe1f2545.png"
+  },
+  {
+    "revision": "862197dcb49161919352f2506af58bd4",
+    "url": "/urband/static/media/record.862197dc.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/urband/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/urband/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/urband/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "cbce03ba1494f0c12d78935160f181e8",
+    "url": "/urband/static/media/tim-icon-.cbce03ba.eot"
+  },
+  {
+    "revision": "10c84345e3d7a4ad4040666b2fc37593",
+    "url": "/urband/static/media/tim-icon.10c84345.woff"
+  },
+  {
+    "revision": "10c84345e3d7a4ad4040666b2fc37593",
+    "url": "/urband/static/media/tim-icon.10c84345.woff2"
+  },
+  {
+    "revision": "cbce03ba1494f0c12d78935160f181e8",
+    "url": "/urband/static/media/tim-icon.cbce03ba.eot"
+  },
+  {
+    "revision": "cd22880fda2f5f5ad1903589d8602b40",
+    "url": "/urband/static/media/tim-icon.cd22880f.ttf"
+  },
+  {
+    "revision": "a9e21a28edeb19fb17248b8d586586fb",
+    "url": "/urband/static/media/vid-1.a9e21a28.jpg"
+  }
+]);
